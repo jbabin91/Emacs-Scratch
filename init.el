5bdbfaf6ef4d4cc65cdf03d7506adeb87e5f275e
Jace Babin <jbabin91@gmail.com>
2021-07-28 00:04:22 -0700

    Starting a Emacs personal configuration

---
+++ init.el
@@ -0,0 +1,20 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("6b1abd26f3e38be1823bd151a96117b288062c6cde5253823539c6926c3bb178" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" default))
+ '(global-command-log-mode t)
+ '(package-selected-packages
+   '(forge evil-magit counsel-projectile hydra helpful fish-mode protobuf-mode bazel-mode toml-mode dockerfile-mode yaml-mode csharp-mode typescript-mode blacken elm-mode gotest go-snippets go-mode rust-mode ormolu lsp-haskell haskell-snippets company-lsp lsp-ivy lsp-ui lsp-mode company visual-regexp magit-libgit libgit deadgrep flycheck-inline flycheck rainbow-delimiters org-bullets neotree magit markdown-mode lua-mode haskell-mode ivy-posframe sudo-edit peep-dired dired-open all-the-icons-dired emojify elfeed-goodies elfeed dashboard gcmh general evil-tutor doom-modeline evil-collection evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
